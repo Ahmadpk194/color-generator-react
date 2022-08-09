d6a1a98ae27d0ffb0d44da97ecb83634fe3841a2
Ahmad khan <Ahmadpk194@gmail.com>
2022-08-09 05:51:48 -0700

    fist commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      &copy;Copyrighted&nbsp; by Ahmad khan{" "}
+      <small>
+        <sup>9 jun 2022</sup>
+      </small>
+    </footer>
+  );
+}
+
+export default Footer;
